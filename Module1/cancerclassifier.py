150d8795080e54c1a91d5b8359529b6420162d66
nicholasprayogo <nprayogo@ryerson.ca>
2018-07-07 22:09:47 -0400

    Included proper function names and display optimum k value for highest accuracy

--- Module1/cancerclassifier.py
+++ Module1/cancerclassifier.py
@@ -11,6 +11,8 @@ import pandas as pd
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt
+
 
 cancer = load_breast_cancer()
 
@@ -20,7 +22,7 @@ print(cancer.keys())
 
 # You should write your whole answer within the function provided. The autograder will call
 # this function and compare the return value against the correct solution value
-def answer_zero():
+def no_features():
     # This function returns the number of features of the breast cancer dataset, which is an integer. 
     # The assignment question description will tell you the general format the autograder is expecting
     return len(cancer['feature_names'])
@@ -30,7 +32,9 @@ def answer_zero():
 
 #print(np.concatenate((cancer['data'],cancer['target']),axis=1))
 
-def answer_one(cancer):
+
+#display data
+def disp_data(cancer):
     df=pd.DataFrame(cancer['data'], columns=['mean radius', 'mean texture', 'mean perimeter', 'mean area',
                                      'mean smoothness', 'mean compactness', 'mean concavity',
                                      'mean concave points', 'mean symmetry', 'mean fractal dimension',
@@ -47,41 +51,55 @@ def answer_one(cancer):
     return(a)
 
 
-dataset=answer_one(cancer)
+dataset=disp_data(cancer)
 print(dataset)
 
+#create new variable for labels (y values)
 labels=dataset['target']
 
-def answer_two():
+
+def class_count():
+    #change from boolean to string for interpretability
     def categorise():
         label=labels.replace(value="malignant",to_replace=1).replace(value="benign",to_replace=0)
         return(label)
     label=categorise()
     print(label.rename('target'))
+
+    #count number of occurences of each class (in this case malignant and benign)
     target=label.value_counts()
     return(target)
 
-print(answer_two())
+print(class_count())
 
 
-
-def answer_three():
+#Distinguish between input and output/target values
+def x_y_vals():
     X=pd.DataFrame(dataset.iloc[:,:30])
     #remember iloc is integer-based
     y=labels
     return((X,y))
 
-def answer_four():
-    X, y = answer_three()
+#Split into train and test set with 0 random state (can tweak)
+def split():
+    X, y = x_y_vals()
     X_train, X_test, y_train, y_test=train_test_split(X,y,random_state=0)
     #print(y_train)
     return(X_train, X_test, y_train, y_test)
 
-def answer_five():
-    X_train, X_test, y_train, y_test = answer_four()
-    knn=KNeighborsClassifier(n_neighbors=1)
+
+def fit_data():
+    X_train, X_test, y_train, y_test = split()
+    #create classifier instance
+    knn=KNeighborsClassifier(n_neighbors=50)
+
+    #keep tweaking number of neighbours
+    #accuracy increases as n_neighbours increases
+
+
     #print(knn.score(X_test,y_test))
-    # Your code here
+
+    #Fit classifier to data
     return(knn.fit(X_train,y_train))
 
 
@@ -90,11 +108,12 @@ def answer_five():
 #means = cancerdf.mean()[:-1].values.reshape(1, -1)
 #print(means)
 
-def answer_six():
-    cancerdf = answer_one(cancer)
+#Predict based on average of all features (not really serving the purpose of the classifier)
+def mean_pred():
+    cancerdf = disp_data(cancer)
     means = cancerdf.mean()[:-1].values.reshape(1,-1)
-    print(means.shape)
-    knn=answer_five()
+    print(means)
+    knn=fit_data()
     print(knn)
     mean_predict=knn.predict(means)
     
@@ -104,29 +123,30 @@ def answer_six():
     else:
         mean_predict="benign"
     return mean_predict
-print("Prediction for average of all features:", answer_six())
+print("Prediction for average of all features:", mean_pred())
 
-def answer_seven():
-    X_train, X_test, y_train, y_test = answer_four()
-    knn = answer_five()
+#Predict based on test data
+def test_pred():
+    X_train, X_test, y_train, y_test = split()
+    knn = fit_data()
     test_predict0=knn.predict(X_test)
     test_predict=pd.Series(test_predict0)
     test_predict=test_predict.replace(value="malignant",to_replace=1).replace(value="benign",to_replace=0)
     return(test_predict)
-print("Prediction for test data:\n", answer_seven() )
+print("Prediction for test data:\n", test_pred() )
 
-def answer_eight():
-    X_train, X_test, y_train, y_test = answer_four()
-    knn = answer_five()
+def classifier_score():
+    X_train, X_test, y_train, y_test = split()
+    knn = fit_data()
     knn_score=knn.score(X_test,y_test)
     return (knn_score)
 
-print("Prediction accuracy:", answer_eight())
+print("Prediction accuracy:", classifier_score())
 
 def accuracy_plot():
-    import matplotlib.pyplot as plt
+
     
-    X_train, X_test, y_train, y_test = answer_four()
+    X_train, X_test, y_train, y_test = split()
 
     # Find the training and testing accuracies by target value (i.e. malignant, benign)
     mal_train_X = X_train[y_train==0]
@@ -139,7 +159,7 @@ def accuracy_plot():
     ben_test_X = X_test[y_test==1]
     ben_test_y = y_test[y_test==1]
 
-    knn = answer_five()
+    knn = fit_data()
 
     scores = [knn.score(mal_train_X, mal_train_y), knn.score(ben_train_X, ben_train_y), 
               knn.score(mal_test_X, mal_test_y), knn.score(ben_test_X, ben_test_y)]
@@ -165,10 +185,39 @@ def accuracy_plot():
 
     plt.xticks([0,1,2,3], ['Malignant\nTraining', 'Benign\nTraining', 'Malignant\nTest', 'Benign\nTest'], alpha=0.8);
     plt.title('Training and Test Accuracies for Malignant and Benign Cells', alpha=0.8)
-    
+
+    plt.show()
     return
 
 accuracy_plot()
+max_value=[0]
+k_optim=[]
+k_vals=[]
+knn_scores=[]
+X_train, X_test, y_train, y_test = split()
+for k in range(1, 100):
+    knn=KNeighborsClassifier(n_neighbors=k)
+    fit=knn.fit(X_train, y_train)
+    knn_score = knn.score(X_test, y_test)
+    k_vals.append(k)
+    knn_scores.append(knn_score)
+
+    #attempt to store max value with its corresponding k value
+
+    if knn_score>max(max_value):
+        max_value.append(knn_score)
+        k_optim.append(k)
+    print("Score for %s neighbours:" %k, knn_score)
+
+print("Optimized k value is {}".format(k_optim[-1]), "with accuracy of : {}".format(max(max_value)))
+
+#make a dataframe to plot values (one way of plotting)
+k_df= pd.DataFrame({'k': k_vals, 'scores': knn_scores})
+# we use dictionaries in this case to build the dataframe
+
+k_df.plot('k', 'scores', kind='line')
+
+plt.show()
 
 
 
